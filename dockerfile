b56a2e3096112143736d3777b2309e72b020185b
zouxingwei <as26623111@163.com>
2020-10-26 22:30:45 +0800

    再度更新

--- dockerfile
+++ dockerfile
@@ -1,4 +1,3 @@
 FROM nginx
 COPY ./index.html /usr/share/nginx/html/
-COPY ./dist /usr/share/nginx/html/dist
 EXPOSE 80
--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(async ctx => {
-   ctx.body = 'Hello docker';
-});
-
-app.listen(3000);
